4674a95a6dac5efe3eedb38c172caa91e0a13ed9
chima <chimajunior14@gmail.com>
2025-02-21 14:19:12 +0000

    added recommendation routes

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const movieRoutes = require('./routes/movieRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
+const recommendRoutes = require('./routes/recommendRoutes'); 
+
 
 const app = express();
 app.use(cors());
@@ -13,6 +15,8 @@ app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/movies', movieRoutes);
 app.use('/api/reviews', reviewRoutes);
+app.use('/api/recommendations', recommendRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
