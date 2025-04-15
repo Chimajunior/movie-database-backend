03037db94a60ad1903cfde96bb55a12212ec66b7
chima <chimajunior14@gmail.com>
2025-04-15 02:59:45 +0100

    new db

--- db.js
+++ db.js
@@ -1,13 +1,22 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT || 3306,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// }).promise();
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 3306, // Make sure this line is here
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 }).promise();
+
 
 pool.getConnection()
   .then(() => console.log("✅ Connected to MySQL!"))
